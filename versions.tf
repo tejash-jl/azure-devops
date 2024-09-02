c52d5384c53f5209680e1c1ac88432160e232b1a
Kumar M <pkumarm@deloitte.com>
2024-09-02 12:31:06 +0530

    azur

--- versions.tf
+++ versions.tf
@@ -2,13 +2,24 @@
 # SPDX-License-Identifier: MPL-2.0
 
 terraform {
+  required_version = ">=0.12"
+
   required_providers {
+    azapi = {
+      source  = "azure/azapi"
+      version = "~>1.5"
+    }
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "3.67.0"
+      version = "~>3.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~>3.0"
+    }
+    time = {
+      source  = "hashicorp/time"
+      version = "0.9.1"
     }
   }
-
-  required_version = ">= 0.14"
 }
-
